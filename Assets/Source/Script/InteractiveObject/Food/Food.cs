ee98bb4efa4bbc9fb28783464f205035fdfd3584
Kirill Larionov <strrranger1@gmail.com>
2022-11-14 23:58:21 +0300

    Little food fix & change version

--- Assets/Source/Script/InteractiveObject/Food/Food.cs
+++ Assets/Source/Script/InteractiveObject/Food/Food.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 
+
+[RequireComponent(typeof(Collider))]
 public class Food : MonoBehaviour
 {
     [SerializeField] private float _requiredSize;
     [SerializeField] private float _price;
+
+    private Collider _collider;
 
     public float RequiredSize { get; private set; }
     public float Price { get; private set; }
@@ -12,6 +16,7 @@ public class Food : MonoBehaviour
     {
         RequiredSize = _requiredSize;
         Price = _price;
+        _collider = GetComponent<Collider>();
     }
 
     private void OnTriggerEnter(Collider collider)
@@ -19,6 +24,8 @@ public class Food : MonoBehaviour
         if(collider.TryGetComponent<FoodPickuper>(out FoodPickuper component))
         {
             component.Pickup(this);
+            _collider.enabled = false;
+
         }
     }
 }
