e6d174799c0a19c7ea37da7c3a8e788e26f109c7
Kirill Larionov <strrranger1@gmail.com>
2022-11-25 03:24:16 +0300

    Added fullscreen add

---
+++ Assets/Source/Script/Web-LG/YandexSDK/FullscreenAdd.cs
@@ -0,0 +1,41 @@
+using Agava.YandexGames;
+using System.Collections;
+using UnityEngine;
+using Zenject;
+
+public class FullscreenAdd : MonoBehaviour
+{
+    [SerializeField] private ButtonClickReader _nextLevelButton;
+
+    void Awake()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+StartCoroutine(ShowStartAdd());
+#endif
+    }
+
+    private void OnEnable()
+    {
+        _nextLevelButton.ButtonClicked += OnButtonClicked;
+    }
+
+    private void OnDisable()
+    {
+        _nextLevelButton.ButtonClicked += OnButtonClicked;
+
+    }
+
+    private IEnumerator ShowStartAdd()
+    {
+        yield return YandexGamesSdk.Initialize();
+
+        InterstitialAd.Show();
+    }
+
+    private void OnButtonClicked()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        InterstitialAd.Show();
+#endif
+    }
+}
