57fadc2ca20b1d29b9956a0032994736341b2589
Kirill Larionov <strrranger1@gmail.com>
2022-12-02 19:47:29 +0300

    Change money pickup animation

--- Assets/Source/Script/InteractiveObject/Money/MoneyPickuper.cs
+++ Assets/Source/Script/InteractiveObject/Money/MoneyPickuper.cs
@@ -5,11 +5,13 @@ using UnityEngine.Events;
 public class MoneyPickuper : MonoBehaviour
 {
     [SerializeField] private float _pickupDuration;
+    [SerializeField] private float _scaler;
 
     public event UnityAction MoneyPickedUp;
 
     public void Pickup(Money money)
     {
+        money.transform.DOScale(_scaler, _pickupDuration);
         money.transform.DOMove(transform.position, _pickupDuration).OnComplete(() => OnPickupComplete(money));
     }
 
